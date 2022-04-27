089e14c2abf034cc8cec5ec0307c1e91fd7af16b
alescrocaro <alescrocaro@gmail.com>
2022-04-27 11:45:55 -0300

    index do backend iniciado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-
-app.get('/', (req,res) => {
-    res.send('hello world!')
+app.get('/posts', (req, res) => {
+    return res.json({
+        post_id: '1',
+        post_title: 'Macaco fora do lugar',
+        post_description: 'Macaco estava no chão invés de estar numa árvore'
+    });
 })
 
-app.listen(3001, () => {
-    console.log('server on')
+app.listen(3333, () => {
+    console.log('server on');
 })
