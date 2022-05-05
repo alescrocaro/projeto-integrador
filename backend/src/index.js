b67a1ab1f87d44d8d16e3bc6ddadf319327b2fd4
alescrocaro <alescrocaro@gmail.com>
2022-05-05 11:25:00 -0300

    :heavy_plus_sign: Adicionando CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes');
 
 app.use(express.json());
+app.use(cors({
+    origin: '*',
+  }));
 app.use(routes);
 
 app.listen(3333, () => {
