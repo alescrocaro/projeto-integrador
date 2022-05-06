398af7c26147e132a6c3f8160e13a100980774c0
alescrocaro <alescrocaro@gmail.com>
2022-05-06 20:50:43 -0300

    :sparkles: verifica se a pag existe
    Se a página não existir vai mostrar uma mensagem com tal informação

--- frontend/src/Pages/Post/specificPost.js
+++ frontend/src/Pages/Post/specificPost.js
@@ -16,148 +16,154 @@ import Button from '@mui/material/Button';
 export default function SpecificPost() {  
   const [post, setPost] = useState({});
   const {id} = useParams();
-  console.log(id);
+  console.log('ID::::::::::::',id);
 
 
   useEffect(() => {
     const fetch = async () => {
       const { data } = await api.get('/posts/'+id);
       setPost(data);
-      console.log('info do post', data);
+      console.log('info do post:::::::::::', data);
     };
     fetch();
   }, []);
 
   return (
     <Layout>
-      <Container container>
-        <div 
-          class='container-content'
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%',
-          }}
-        >
-          <HeaderPage 
-            title={post.id} 
-            userName={post.userName} 
-            dateFound={post.dateFound} 
-          />
+      {!!post &&
+        <Container container>
           <div 
-            class='specimen-local'
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'flex-start',
-            }}
-          >
-            <div 
-              class='specimen'
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                maxWidth: '50%',
-                backgroundColor: 'gray',
-              }}
-            >
-              <Typography variant='h5' color='black'>
-                ESPÉCIME:
-              </Typography>
-              <img 
-                src={require('../../img/foto1.jpg')} 
-                alt='img'
-                style={{
-                  maxWidth: '90%',
-                  margin: '10px' 
-                }}
-              />
-              <Typography variant='h7' color='black'>
-                CLASSIFICAÇÃO CIENTÍFICA:
-              </Typography>
-              <StyledTable data={post} scientificTable />
-            </div>
-            <div 
-              class='local'
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                maxWidth: '50%',
-                backgroundColor: 'blue',
-              }}
-            >
-              <Typography variant='h5' color='black'>
-                LOCAL E DATA:
-              </Typography>
-              <img 
-                src={require('../../img/foto1.jpg')} 
-                alt='img'
-                style={{
-                  maxWidth: '90%',
-                  margin: '10px' 
-                }}
-              />
-              <Typography variant='h7' color='black'>
-                DETALHES:
-              </Typography>
-              <StyledTable data={post} detailsTable />
-              
-            </div>
-          </div>
-          <div 
-            class='description'
-            style={{
-              backgroundColor: 'pink',
-              margin: '10px 0',
-            }}
-          >
-            <Typography variant='h5' color='black'>
-              DESCRIÇÃO:
-            </Typography>
-            <Typography variant='h8' color='black'>
-              {post.description}
-            </Typography>
-
-          </div>
-          <div 
-            class='community'
+            className='container-content'
             style={{
               display: 'flex',
               flexDirection: 'column',
-              backgroundColor: 'yellow',
+              width: '100%',
             }}
           >
-            <Typography variant='h5' color='black'>
-              COMUNIDADE:
-            </Typography>
-            <Typography variant='h7' color='black'>
-              COMENTARIO 1:
-            </Typography>
-
-            <Typography variant='h7' color='black'>
-              CONTESTAÇÃO:
-            </Typography>
-            <Typography variant='h5' color='black'>
-              ESCREVER COMENTÁRIO:
-            </Typography>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <div
+            <HeaderPage 
+              title={post.id} 
+              userName={post.userName} 
+              dateFound={post.dateFound} 
+            />
+            <div 
+              className='specimen-local'
               style={{
                 display: 'flex',
                 flexDirection: 'row',
-                justifyContent: 'flex-end'
+                alignItems: 'flex-start',
               }}
             >
-              <Button variant='contained' color='error'>CONTESTAR</Button>
-              <Button variant='contained' color='primary'>COMENTAR</Button>
+              <div 
+                className='specimen'
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  maxWidth: '50%',
+                  backgroundColor: 'gray',
+                }}
+              >
+                <Typography variant='h5' color='black'>
+                  ESPÉCIME:
+                </Typography>
+                <img 
+                  src={require('../../img/foto1.jpg')} 
+                  alt='img'
+                  style={{
+                    maxWidth: '90%',
+                    margin: '10px' 
+                  }}
+                />
+                <Typography variant='h7' color='black'>
+                  CLASSIFICAÇÃO CIENTÍFICA:
+                </Typography>
+                <StyledTable data={post} scientificTable />
+              </div>
+              <div 
+                className='local'
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  maxWidth: '50%',
+                  backgroundColor: 'blue',
+                }}
+              >
+                <Typography variant='h5' color='black'>
+                  LOCAL E DATA:
+                </Typography>
+                <img 
+                  src={require('../../img/foto1.jpg')} 
+                  alt='img'
+                  style={{
+                    maxWidth: '90%',
+                    margin: '10px' 
+                  }}
+                />
+                <Typography variant='h7' color='black'>
+                  DETALHES:
+                </Typography>
+                <StyledTable data={post} detailsTable />
+                
+              </div>
+            </div>
+            <div 
+              className='description'
+              style={{
+                backgroundColor: 'pink',
+                margin: '10px 0',
+              }}
+            >
+              <Typography variant='h5' color='black'>
+                DESCRIÇÃO:
+              </Typography>
+              <Typography variant='h8' color='black'>
+                {post.description}
+              </Typography>
+
+            </div>
+            <div 
+              className='community'
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                backgroundColor: 'yellow',
+              }}
+            >
+              <Typography variant='h5' color='black'>
+                COMUNIDADE:
+              </Typography>
+              <Typography variant='h7' color='black'>
+                COMENTARIO 1:
+              </Typography>
+
+              <Typography variant='h7' color='black'>
+                CONTESTAÇÃO:
+              </Typography>
+              <Typography variant='h5' color='black'>
+                ESCREVER COMENTÁRIO:
+              </Typography>
+              <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'flex-end'
+                }}
+              >
+                <Button variant='contained' color='error'>CONTESTAR</Button>
+                <Button variant='contained' color='primary'>COMENTAR</Button>
+              </div>
             </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      }
+      {!!!post && 
+        <h1>Pagina nao encontrada</h1>
+      
+      }
     </Layout>
   );
 };
