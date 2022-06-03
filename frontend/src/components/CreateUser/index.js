547122045d34baf6171a557692cf1ebcf75548e7
alescrocaro <alescrocaro@gmail.com>
2022-06-03 17:18:58 -0300

    :sparkles: adicionando verificação de criação de conta de usuário
    formik+yup

--- frontend/src/components/CreateUser/index.js
+++ frontend/src/components/CreateUser/index.js
@@ -1,40 +1,75 @@
 import * as React from 'react';
+
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import api from '../../services/api';
+
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import OutlinedInput from '@mui/material/OutlinedInput';
+import TextField from '@mui/material/TextField';
 
-export default function FormDialog() {
+
+export default function SignUpDialog() {
+  const navigate = useNavigate();
   const [open, setOpen] = React.useState(false);
-  const [values, setValues] = React.useState({
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
-    showPassword: false,
+
+  async function redirect(){
+    navigate('/');
+  }
+  
+  const scheme = Yup.object().shape({
+    email: Yup.string().required('É necessário inserir um endereço de email!'),
+    name: Yup.string().required('É necessário inserir seu nome'),
+    lastName: Yup.string().required('É necessário inserir seu sobrenome'),
+    password: Yup.string()
+      .min(6, ({ min }) => `A senha deve ter no mínimo ${min} caracteres`)
+      .required('É necessário inserir uma senha!'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'As senhas não são iguais!')
+      .required('É necessário confirmar sua senha!'),
+  })
+
+  const formik = useFormik({
+    validationSchema: scheme,
+    initialValues: {
+      email: '',
+      name: '',
+      lastName: '',
+      userDescription: '',
+      password: '',
+      confirmPassword: '',
+    },
+    onSubmit: async (values) => {
+      try {
+        const { email, name, lastName, userDescription = '', password } = values;
+
+        console.log((email));
+
+        await api.post('/user', {
+          email, 
+          name,
+          lastName,
+          userDescription,
+          password,
+        });
+
+
+        handleClose();
+        alert(`Sua conta foi criada. Obrigado por se juntar ao Invasores, ${name}!`);
+
+        redirect();
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    }
   });
 
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-  
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
-    });
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -51,59 +86,93 @@ export default function FormDialog() {
       </Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Criar conta</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Para criar uma conta de usuário preencha os campos abaixo.
-          </DialogContentText>
-          <OutlinedInput
-            autoFocus
-            margin="dense"
-            id="email"
-            placeholder="Endereço de email"
-            type="email"
-            fullWidth
-            variant="standard"
-            sx={{mt: 1}}
-          />
-          <OutlinedInput
-            autoFocus
-            margin="dense"
-            id="user"
-            placeholder="Usuário"
-            type="text"
-            fullWidth
-            variant="standard"
-            sx={{mt: 1}}
-          />
-          <OutlinedInput
-            autoFocus
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            placeholder="Senha"
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            sx={{
-              width: '100%',
-              mt: 1
-            }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button variant="outlined" color="error" onClick={handleClose}>Cancelar</Button>
-          <Button variant="outlined" onClick={handleClose}>Criar</Button>
-        </DialogActions>
+        <form onSubmit={formik.handleSubmit}>
+          <DialogContent>
+            <DialogContentText>
+              Para criar uma conta de usuário preencha os campos abaixo.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="email"
+              variant="outlined"
+              label="Endereço de email"
+              type="email"
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              fullWidth
+              sx={{mt: 1}}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              variant="outlined"
+              label="Nome"
+              error={formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
+              onChange={formik.handleChange}
+              value={formik.values.name}
+              fullWidth
+              sx={{mt: 1}}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="lastName"
+              label="Sobrenome"
+              type="text"
+              variant="outlined"
+              error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+              helperText={formik.touched.lastName && formik.errors.lastName}
+              onChange={formik.handleChange}
+              value={formik.values.lastName}
+              fullWidth
+              sx={{mt: 1}}
+            />
+            <TextField
+              autoFocus
+              id="password"
+              label="Senha"
+              type="password"
+              variant="outlined"
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              fullWidth
+              sx={{mt: 1}}
+            />
+            <TextField
+              autoFocus
+              id="confirmPassword"
+              label="Confirmar senha"
+              type="password"
+              variant="outlined"
+              error={
+                formik.touched.confirmPassword && 
+                Boolean(formik.errors.confirmPassword)
+              }
+              helperText={
+                formik.touched.confirmPassword && formik.errors.confirmPassword
+              }
+              onChange={formik.handleChange}
+              value={formik.values.confirmPassword}
+              fullWidth
+              sx={{mt: 1}}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button variant="outlined" color="error" onClick={handleClose}>
+              Cancelar
+            </Button>
+            <Button variant="outlined" type="submit">
+              Criar
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
