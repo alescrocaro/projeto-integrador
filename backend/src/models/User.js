26417c6a7b8224ae6911106031314a1785bb2126
leoomori <leoomorifaris@gmail.com>
2022-06-14 20:52:48 -0300

    Add mostrar usuário nos posts

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     salt: DataTypes.STRING,
-    bio: DataTypes.STRING
+    bio: DataTypes.STRING,
 }, {
   sequelize,
   modelName: 'User',
