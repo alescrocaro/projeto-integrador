d56668ccb2e4e70b88f7c658635e7e9c7b9b3ff1
alescrocaro <alescrocaro@gmail.com>
2024-02-25 17:14:36 -0300

    Fix: lint and Update: get filtered posts function

--- frontend/src/Pages/ListPost/index.js
+++ frontend/src/Pages/ListPost/index.js
@@ -1,55 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { api } from '../../services/api';
-import { useToken } from '../../Context/AuthContext';
-
-import Container from '../../components/Container';
-import Layout from '../../components/Layout';
-import StyledButton from '../../components/Button';
-import StyledCard from '../../components/Card';
-import HeaderPage from '../../components/HeaderPage';
-
-import Map from './components/Map';
+import React, { useEffect, useState } from "react";
+import { useToken } from "../../Context/AuthContext";
+import StyledButton from "../../components/Button";
+import StyledCard from "../../components/Card";
+import Container from "../../components/Container";
+import HeaderPage from "../../components/HeaderPage";
+import Layout from "../../components/Layout";
+import { api } from "../../services/api";
+import Map from "./components/Map";
 
 export default function ListPosts() {
   const { user } = useToken();
-  //map filter constrols
+  //map filter controls
   const [mapCenter, setMapCenter] = useState([-15, -48]); //tem que ficar onde esta o mapa e o headerpage
   const [mapSearchRadius, setMapSearchRadius] = useState(12);
   const [mapShowRadius, setMapShowRadius] = useState(false);
-  
-  const mapControls = new Object();
-  mapControls.setMapCenter = (c) => {setMapCenter(c)};
-  mapControls.getMapCenter = () => {return mapCenter};
-  mapControls.setSearchRadius = (r) => {setMapSearchRadius(r)}; 
-  mapControls.getSearchRadius = () => {return mapSearchRadius};
-  mapControls.setShowRadius = (r) => {setMapShowRadius(r)}; 
-  mapControls.getShowRadius = () => {return mapShowRadius};
-
-  //filter controls
-  const applyFilters = async filters => {
-    try {
-      const { data } = await api.get('/posts', { params: filters });
-      setPosts(data);
-    } catch (error) {
-      console.log('error getting posts', error);
-    }
-  };
-
-  // //funcao para setar o mapcenter (latlng)
-  // const handleMapCenter = (c) => {setMapCenter(c)}; //tem que ser enviado pro mapa, e recebido aqui por meio de props
-
   const [posts, setPosts] = useState([]);
 
+  const mapControls = new Object({
+    getMapCenter: () => {
+      return mapCenter;
+    },
+    setMapCenter: (c) => {
+      setMapCenter(c);
+    },
+    getSearchRadius: () => {
+      return mapSearchRadius;
+    },
+    setSearchRadius: (r) => {
+      setMapSearchRadius(r);
+    },
+    getShowRadius: () => {
+      return mapShowRadius;
+    },
+    setShowRadius: (r) => {
+      setMapShowRadius(r);
+    },
+  });
+
+  const getPosts = async (filters) => {
+    await api.get("/posts", { params: filters }).then(({ data }) => {
+      setPosts(data);
+    });
+  };
+
   useEffect(() => {
-    const fetch = async () => {
-      try {
-        const { data } = await api.get('/posts');
-        setPosts(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetch();
+    getPosts();
   }, []);
 
   return (
@@ -57,28 +52,30 @@ export default function ListPosts() {
       <Container
         container
         sx={{
-          minHeight: '0 !important'
+          minHeight: "0 !important",
         }}
       >
         {user && (
           <StyledButton
-            title={'ADICIONAR NOVA OBSERVAÇÃO'}
-            icon={'add'}
+            title={"ADICIONAR NOVA OBSERVAÇÃO"}
+            icon={"add"}
             isLinkActive
           />
         )}
-        
+
         <Map posts={posts} mapControls={mapControls} />
 
         <HeaderPage
           title="Mostrando observações recentes:"
           filter
           mapControls={mapControls}
-          applyFilters={applyFilters}
+          applyFilters={getPosts}
         />
-        {posts.map(post => (
-          <StyledCard key={post.id} post={post} />
-        ))}
+        {posts.length ? (
+          posts.map((post) => <StyledCard key={post.id} post={post} />)
+        ) : (
+          <span>nenhuma observação encontrada</span>
+        )}
       </Container>
     </Layout>
   );
