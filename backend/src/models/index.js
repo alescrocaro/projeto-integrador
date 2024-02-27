8693692b7c40018746e6f518e1ff9a43f3216982
alescrocaro <alescrocaro@gmail.com>
2024-02-26 21:35:30 -0300

    Update: envs for docker compose

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -19,8 +19,7 @@ const authenticateSequelizeConnection = async () => {
   }
 }
 
-// let sequelize = new Sequelize(`${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
-let sequelize = new Sequelize(`postgres://admin:123456@localhost:5437/forumDB`);
+let sequelize = new Sequelize(process.env.DB_URL);
 
 authenticateSequelizeConnection();
 
