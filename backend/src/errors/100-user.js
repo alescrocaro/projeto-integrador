5d33b2fdb7b28c5f0b7888323f4213fa67a94cc9
alescrocaro <alescrocaro@gmail.com>
2024-03-06 22:35:18 -0300

    Add: missing user error messages

--- backend/src/errors/100-user.js
+++ backend/src/errors/100-user.js
@@ -1,6 +1,13 @@
 const user_errors = {
     "100": "Email already registered",
     "101": "User not found",
+    "102": "Error getting user",
+    "103": "Password must be a minimum of 6 characters and a maximum of 30",
+    "104": "Must provide name",
+    "105": "Must provide email",
+    "106": "Must provide password",
+    "107": "Error creating user",
+    "108": "Invalid password"
 };
 
 module.exports = { user_errors };
