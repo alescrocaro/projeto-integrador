5c423d6d1921cb21b9e18fd4deac6a7f3548b770
alescrocaro <alescrocaro@gmail.com>
2024-04-07 23:00:59 -0300

    #127 - Add: validate if body keys exists in model

--- backend/src/entities/post/post.validator.js
+++ backend/src/entities/post/post.validator.js
@@ -1,9 +1,22 @@
 const validator = require("validator");
 const { validation_errors } = require("../../errors/0-validation");
 const { post_errors } = require("../../errors/200-post");
+const { Post } = require("../../models");
+
+function validatePostBody(req, res) {
+  const keys = Object.keys(req.body);
+  const modelAttributes = Object.keys(Post.rawAttributes);
+
+  const invalidKeys = keys.filter(key => !modelAttributes.includes(key));
+
+  if (invalidKeys.length) {
+    return res.status(400).json({ message: `BAD USER INPUT. The following fields doesn't exist in Post model: ${invalidKeys.join(', ')}` });
+  }
+}
 
 function validateUpdatePost(req, res, next) {
-  console.log('validating')
+  validatePostBody(req, res);
+
   if (!validator.isUUID(req.params.id)) {
     return res
       .status(400)
