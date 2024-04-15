a1cc7f29ec9d4fb4ec4fc9470fcc74724db18ff8
alescrocaro <alescrocaro@gmail.com>
2024-04-15 19:52:22 -0300

    #127 - Add: authenticate token function to jwt service

--- backend/src/services/jwtService.js
+++ backend/src/services/jwtService.js
@@ -1,13 +1,33 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-module.exports = {
-    generateJwt(id, name, email) {
-        const jwtSecret = process.env.JWT_SECRET
-        const token = jwt.sign({
-            id:id, 
-            name, 
-            email:email
-        }, jwtSecret);
-        return token
-    }
+function generateJwt(id, name, email) {
+  const jwtSecret = process.env.JWT_SECRET;
+  const token = jwt.sign(
+    {
+      id: id,
+      name,
+      email: email,
+    },
+    jwtSecret
+  );
+  return token;
 }
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(403);
+    }
+    req.user_id = data.id;
+    next();
+  });
+}
+
+module.exports = { generateJwt, authenticateToken };
